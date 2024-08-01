9e550dfe4e548fb49c7e81dfdd447f6bdda5be96
Yujiin <jiin4083@naver.com>
2024-08-01 09:06:09 +0900

    369 게임

---
+++ swea_IM/240801_swea.py
@@ -0,0 +1,17 @@
+T = 1
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+
+    N = int(input())
+    result = []
+    for n in range(1, N + 1):
+        n = str(n)
+        count = 0
+        if n in '369':
+            count += 1
+        if count > 0:
+            result.append('-')
+        else:
+            result.append(n)
+
+    print(' '.join(result))

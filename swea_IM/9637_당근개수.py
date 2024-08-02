1056a3c8de3bddd16b53b2cbb1cc2d7519e40dcc
Yujiin <jiin4083@naver.com>
2024-08-02 22:09:21 +0900

    당근 개수 왜 자꾸 testcase 5개만 ..?

---
+++ swea_IM/9637_당근개수.py
@@ -0,0 +1,23 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N = int(input())    # 당근개수
+    C = list(map(int, input().split()))  # 당근의 크기
+
+    C.append(0)
+    # print(C)
+
+    # 중복제거
+    count = 0   # 제일 큰 카운트
+    now = 1     # 현재
+
+    for i in range(0, N):
+        if C[i] + 1 == C[i+1]:
+            now += 1
+        else:
+            if count < now:
+                count = now
+            now = 1
+
+    print(f'#{test_case} {count}')
+

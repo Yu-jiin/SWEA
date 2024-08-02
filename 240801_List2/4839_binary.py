6f512c0e069b140c894e3d0fc41ca7958be0587d
Yujiin <jiin4083@naver.com>
2024-08-02 09:08:21 +0900

    240802 Start

--- 240801_List2/4839_binary.py
+++ 240801_List2/4839_binary.py
@@ -2,42 +2,42 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     P, A, B = map(int, input().split())
-
-    page = []
-    for n in range(1, P+1):
-        page.append(n)
-    # print(page)
-
-    start = 1
+    # page = []
+    # for n in range(1, P+1):
+    #     page.append(n)
+    start = 0
     end = P
+
     count = 0
     # A
     while start <= end:
         count += 1
         middle = int((start + end) / 2)
-        if page[middle] == A:
+        if middle == A:
             break
-        elif page[middle] > A:
-            end = middle - 1
+        elif middle > A:
+            end = middle
         else:
-            start = middle + 1
-    print(count)
+            start = middle
+    # print(count)
+
     b_count = 0
     # B
     while start <= end:
         b_count += 1
         middle = int((start + end) / 2)
-        if page[middle] == B:
+        print(middle)
+        if middle == B:
             break
-        elif page[middle] > B:
-            end = middle - 1
+        elif middle > B:
+            end = middle
         else:
-            start = middle + 1
-    print(b_count)
+            start = middle
+
     if count < b_count:
-        result = 'B'
-    elif b_count < count:
         result = 'A'
+    elif b_count < count:
+        result = 'B'
     else:
         result = 0
 
--- 240801_List2/bubunsum.py
+++ 240801_List2/bubunsum.py
@@ -4,13 +4,3 @@ A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 for test_case in range(1, T + 1):
     n = len(A)
     N, K = map(int, input().split())
-
-    for i in range(1 << n):
-        sum1 = 0
-        count = 0
-        for j in range(n):
-            if j & (1 << j):
-                sum1 += i
-        if sum1 == K:
-            count += 1
-    print(count)

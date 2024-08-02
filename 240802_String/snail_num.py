78c09dee291964e6639c916e5140eed061f2bb5f
Yujiin <jiin4083@naver.com>
2024-08-02 15:46:14 +0900

    snail_num 집가서 꼭 꼭 해보기 ..

--- 240802_String/snail_num.py
+++ 240802_String/snail_num.py
@@ -5,27 +5,29 @@ for test_case in range(1, T + 1):
 
     box = [[0] * N for _ in range(N)]
 
-    arr = list(range(1, (N**2)+1))
+    # arr = list(range(1, (N**2)+1))
 
     di = [0, 1, 0, -1]
     dj = [1, 0, -1, 0]
+    i = 0   # 행
+    j = 0   # 열
+    box[i][j] = 1
 
-    box[0][0] = 1
-    x = 0   # 행
-    y = 0   # 열
     count = 1
-
+    num = 1
     while count < N**2:
-        num = 1
         for x in range(4):
-            ni = x + di[x]
-            nj = y + dj[x]
+            ni = i + di[x]
+            nj = j + dj[x]
             if 0 <= ni < N and 0 <= nj < N:
                 if box[ni][nj] == 0:
-                    box[ni][nj] = num + 1
                     count += 1
-                    x = ni
-                    y = nj
-                    break
+                    box[ni][nj] = count
+                    i = ni
+                    j = nj
+                    break   # for 문 종료시키고 다시 while 문으로 돌아가기
 
-    print(box)
+    print(f'#{test_case}')
+    for b in box:
+        print(*b)
+

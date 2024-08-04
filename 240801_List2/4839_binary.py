07644ce50354eedd008e26050dd5e17197d32c37
Yujiin <jiin4083@naver.com>
2024-08-04 15:23:06 +0900

    이진탐색 문제 완료

--- 240801_List2/4839_binary.py
+++ 240801_List2/4839_binary.py
@@ -1,42 +1,63 @@
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
+
+    #
+    # def search(target, P):
+    #     start = 1
+    #     end = P
+    #     count = 0
+    #     while start <= end:
+    #         count += 1
+    #         middle = (start + end)//2
+    #         if middle == target:
+    #             return count
+    #         if middle > target:
+    #             end = middle
+    #         else:
+    #             start = middle
+    #     return count
+    #
+    # resultA = search(A, P)
+    # resultB = search(B, PP, A, B = map(int, input().split()))
+    #
+    # if resultA < resultB:
+    #     result = 'A'
+    # elif resultA > resultB:
+    #     result = 'B'
+    # else:
+    #     result = 0
+    #
+    # print(f'#{test_case} {result}')
+
+
+
+
+#  self
     P, A, B = map(int, input().split())
-    # page = []
-    # for n in range(1, P+1):
-    #     page.append(n)
-    start = 0
-    end = P
 
-    count = 0
-    # A
-    while start <= end:
-        count += 1
-        middle = int((start + end) / 2)
-        if middle == A:
-            break
-        elif middle > A:
-            end = middle
-        else:
-            start = middle
-    # print(count)
+    def search(target, P):
+        start = 1
+        end = P
+        count = 0
 
-    b_count = 0
-    # B
-    while start <= end:
-        b_count += 1
-        middle = int((start + end) / 2)
-        print(middle)
-        if middle == B:
-            break
-        elif middle > B:
-            end = middle
-        else:
-            start = middle
+        while start <= end:
+            count += 1
+            middle = (start + end) // 2
+            if middle == target:
+                return count
+            if middle > target:
+                end = middle
+            else:
+                start = middle
+            return count
 
-    if count < b_count:
+    countA = search(A, P)
+    countB = search(B, P)
+
+    if countA < countB:
         result = 'A'
-    elif b_count < count:
+    elif countB < countA:
         result = 'B'
     else:
         result = 0

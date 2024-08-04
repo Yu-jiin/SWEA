df1ac61ce119c87f0061e06ae1f0cb45f23359a2
Yujiin <jiin4083@naver.com>
2024-08-04 17:37:26 +0900

    selection_sort

---
+++ swea_IM/각 정렬 구현/1966_selection_sort.py
@@ -0,0 +1,23 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    # for i in range(N):
+    #     min_idx = i
+    #     for j in range(i+1, N):
+    #         if arr[j] < arr[min_idx]:
+    #             min_idx = j
+    #         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    # #       제발 arr[i], arr[min_idx]다..
+    # result = ' '.join(map(str, arr))
+    # print(f'#{tc} {result}')
+
+    for i in range(N):
+        idx = i
+        for j in range(i+1, N):
+            if arr[j] < arr[idx]:
+                idx = j
+            arr[i], arr[idx] = arr[idx], arr[i]
+    result = ' '.join(map(str, arr))
+    print(f'#{tc} {result}')

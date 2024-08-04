128dc3b07dcabd8ca9a0cd064935b4b21de11d60
Yujiin <jiin4083@naver.com>
2024-08-04 20:03:47 +0900

    ladder self done

--- swea_IM/1210_ladder.py
+++ swea_IM/1210_ladder.py
@@ -1,15 +1,14 @@
 T = 10
-for tc in range(1, T+1):
+for test_case in range(1, T+1):
     tc = int(input())
     N = 100
-    data = [list(map(int, input().split())) for i in range(N)]
-    # print(data)
+    arr = [list(map(int, input().split())) for a in range(N)]
 
-    di = [0, 0, -1]
-    dj = [1, -1, 0]
+    di = [0, 0, -1] # 순서 매우 중요
+    dj = [1, -1, 0] # 좌우 먼저 !! 위 먼저 가면 안되지 붕시야 ㅠㅠ
 
     for j in range(N):
-        if data[99][j] == 2:
+        if arr[99][j] == 2:
             row = 99
             col = j
 
@@ -18,10 +17,11 @@ for tc in range(1, T+1):
             ni = row + di[x]
             nj = col + dj[x]
             if 0 <= ni < N and 0 <= nj < N:
-                if data[ni][nj] == 1:
-                    data[row][col] = 0  # 지나온 곳은 0으로 표시해서 델타가 가지 못하게
+                if arr[ni][nj] == 1:
+                    arr[row][col] = 0
                     row = ni
                     col = nj
                     break
 
     print(f'#{tc} {col}')
+

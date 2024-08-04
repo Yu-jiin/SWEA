c61c6c06015b095a905af12d548616addbb0dffc
Yujiin <jiin4083@naver.com>
2024-08-04 20:13:01 +0900

    정렬

--- swea_IM/각 정렬 구현/1966_bubble_sort.py
+++ swea_IM/각 정렬 구현/1966_bubble_sort.py
@@ -1,16 +1,16 @@
 # 버블정렬
-# T = int(input())
-# for tc in range(1, T + 1):
-#     N = int(input())
-#     arr = list(map(int, input().split()))
-#
-#     for i in range(N-1):
-#         for j in range(N-1-i):
-#             if arr[j] > arr[j+1]:
-#                 arr[j], arr[j+1] = arr[j+1], arr[j]
-#
-#     result = ' '.join(map(str, arr))
-    # print(f'#{tc} {result}')
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    for i in range(N-1):
+        for j in range(N-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    result = ' '.join(map(str, arr))
+    print(f'#{tc} {result}')
 
 # 버블정렬
 T = int(input())
--- swea_IM/각 정렬 구현/1966_selection_sort.py
+++ swea_IM/각 정렬 구현/1966_selection_sort.py
@@ -14,10 +14,10 @@ for tc in range(1, T + 1):
     # print(f'#{tc} {result}')
 
     for i in range(N):
-        idx = i
+        min_idx = i
         for j in range(i+1, N):
-            if arr[j] < arr[idx]:
-                idx = j
-            arr[i], arr[idx] = arr[idx], arr[i]
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
     result = ' '.join(map(str, arr))
     print(f'#{tc} {result}')

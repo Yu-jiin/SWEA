6c390c286700d716942c324f8eb499eb3c8727f4
Yujiin <jiin4083@naver.com>
2024-08-06 10:37:27 +0900

    재귀함수 학습 done

--- 240806_Stack/재귀호출_학습.py
+++ 240806_Stack/재귀호출_학습.py
@@ -1,3 +1,17 @@
+def f(n):
+    global cnt
+    cnt += 1
+    if n == 0:
+        return
+    else:
+        f(n-1)
+
+
+cnt = 0
+f(50)   # 파이파이는 1000번대도 가능 ㅠㅠ
+print(cnt)
+
+
 # 팩토리얼
 def fac(num):
     if num == 1:
@@ -12,7 +26,7 @@ print(fac(5))
 
 # 피보나치
 def fibo(num):
-    if num == 0:
+    if num == 0:    # if num < 2: return n
         return 0
     elif num == 1:
         return 1

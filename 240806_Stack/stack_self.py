2368a48ec9683286f41fde5c89d2cd834470a26e
Yujiin <jiin4083@naver.com>
2024-08-06 17:09:52 +0900

    stack_self done

--- 240806_Stack/stack_self.py
+++ 240806_Stack/stack_self.py
@@ -1,15 +1,18 @@
 T = int(input())
 for tc in range(1, T+1):
     word = input()
-
+    result = -1     # result -1 을 default 로
     stack = []
-
-    for w in word:
-        if w is '(':
-            stack.append(w)
-        elif w is ')':
-            if not stack or stack[-1] == '(':   # ?
+    if word[0] == '(':
+        for w in word:
+            # print(w, stack)
+            if w == '(':
+                stack.append(w)
+            elif w == ')' and '(' == stack[-1]:
                 stack.pop()
-            result = -1
+            elif w == ')':
+                break
         if len(stack) == 0:
             result = 1
+
+    print(f'#{tc} {result}')

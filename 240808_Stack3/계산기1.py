40d500900d04ea3db2fc4307d0df1a43d271b4d1
Yujiin <jiin4083@naver.com>
2024-08-08 17:54:55 +0900

    계산기 후위표기까지 가능

--- 240808_Stack3/계산기1.py
+++ 240808_Stack3/계산기1.py
@@ -6,15 +6,27 @@ def make_postfix(postfix):
     }
     for n in postfix:
         if n.isdigit():
-            result + n
-        elif n 
+            result += n
+        elif n in t.keys():
+            if stack:
+                result += stack[-1]
+                stack.pop()
+            stack.append(n)
 
+    while stack:
+        result += stack[-1]
+        stack.pop()
 
     return result
+
+
+def calcu(result):
+    total = 0
+
 
 T = 10
 for tc in range(1, T+1):
     str_len = int(input())
     postfix = list(map(str,input()))
 
-    make_postfix(postfix)
+    print(make_postfix(postfix))

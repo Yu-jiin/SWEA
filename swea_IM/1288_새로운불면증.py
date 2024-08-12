2f340bcd2550be880158066c5800ad0da46927fc
Yujiin <jiin4083@naver.com>
2024-08-12 20:50:20 +0900

    불면증 done

--- swea_IM/1288_새로운불면증.py
+++ swea_IM/1288_새로운불면증.py
@@ -4,19 +4,18 @@ for tc in range(1, T+1):
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
     n = int(input())
-    number = 1
+    number = 0
     count = 0
     total_count = 0
-    while count < 10:
+    while numbers:
         total_count += 1
+        number += 1
         result = str(n * number)
         for s in result:
             if int(s) in numbers:
                 s = int(s)
                 numbers.remove(s)
-                count += 1
-                number += 1
+                # count += 1
 
-
-    print(f'#{tc} {count}')
+    print(f'#{tc} {result}')
 

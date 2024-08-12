200a3ed6fd33e3ad57c0bb0f4ded762c2ea66c5a
Yujiin <jiin4083@naver.com>
2024-08-12 21:13:29 +0900

    delta

--- swea_IM/12712_파리퇴치3.py
+++ swea_IM/12712_파리퇴치3.py
@@ -4,6 +4,10 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    #
+    # 가로세로 델타
     di = [0, 1, 0, -1]
     dj = [-1, 0, 1, 0]
+
+    # 대각선 델타
+    ci = [1, -1]
+    cj = [1, -1]

29220393ea334a184630cbbdae8ffa3ad8152edf
Yujiin <jiin4083@naver.com>
2024-08-12 23:17:20 +0900

    파리퇴치 done

--- swea_IM/12712_파리퇴치3.py
+++ swea_IM/12712_파리퇴치3.py
@@ -12,4 +12,36 @@ for tc in range(1, T+1):
     ci = [1, -1, -1, 1]
     cj = [1, -1, 1, -1]
 
+    max_value = 0
+    total = 0
+    max_value2 = 0
+    for i in range(N):
+        for j in range(N):
+            target = arr[i][j]
+            for x in range(4):
+                for m in range(1, M):
+                    ni = i + di[x] * m
+                    nj = j + dj[x] * m
+                    if 0 <= ni < N and 0 <= nj < N:
+                        target += arr[ni][nj]
+            if max_value < target:
+                max_value = target
 
+    for i in range(N):
+        for j in range(N):
+            target = arr[i][j]
+            for x in range(4):
+                for m in range(1, M):
+                    ni = i + ci[x] * m
+                    nj = j + cj[x] * m
+                    if 0 <= ni < N and 0 <= nj < N:
+                        target += arr[ni][nj]
+            if max_value2 < target:
+                max_value2 = target
+
+    if max_value > max_value2:
+        total = max_value
+    else:
+        total = max_value2
+
+    print(f'#{tc} {total}')

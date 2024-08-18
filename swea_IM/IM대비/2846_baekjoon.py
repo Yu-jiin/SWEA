5e6b4875fcbb40d47abef5b680ee06c7175b75a1
Yujiin <jiin4083@naver.com>
2024-08-18 13:22:55 +0900

    백준 2846 오르막길 done

--- swea_IM/IM대비/2846_baekjoon.py
+++ swea_IM/IM대비/2846_baekjoon.py
@@ -1,2 +1,18 @@
 N = int(input())
 arr = list(map(int, input().split()))
+arr.append(0)
+# 오르막
+new_list = [arr[0]]
+max_len = 0
+
+for i in range(1, N):
+    if arr[i] > arr[i-1]:
+        new_list.append(arr[i])
+    else:
+        new_list = [arr[i]]
+
+    up = new_list[-1] - new_list[0]
+    if max_len < up:
+        max_len = up
+
+print(max_len)

4e6b6d1ee202d38f95ae01f38c78d532ace10a9a
Yu-jiin <jiin4083@naver.com>
2024-08-30 15:29:21 +0900

    4012 요리사 집가서 하기 !! 대게묵고

---
+++ swea_A/4012.chief.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('4012.txt')
+
+def check_food(start, min_sum):
+    for i in range(1, N):
+        for j in range(1, N):
+            if i == j:
+                continue
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    padding = [0] * (N + 1)
+    arr = [[0] + list(map(int, input().split())) for _ in range(N)]
+    arr.insert(0, padding)
+
+    min_food = float('inf')
+
+    food1 = check_food(1, 0)
+    food2 = check_food(1, 0)

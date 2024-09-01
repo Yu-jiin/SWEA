7cd9983c311c2608020cd9ccf919a8538b1db94e
Yujiin <jiin4083@naver.com>
2024-09-01 22:02:24 +0900

    0901 start

--- remake_Algo/구간합_리메이크/my_practice.py
+++ remake_Algo/구간합_리메이크/my_practice.py
@@ -16,12 +16,23 @@ for tc in range(1, T+1):
         while len(row) < max_row:
             row.append(0)
 
-    for col in zip(*arr):
-        pass
+    max_value = float('-inf')
+    min_value = float('inf')
+    lst = []
+    for j in range(max_row):
+        for i in range(N):
+            sum_v = 0
+            count = 0
+            for x in range(i, N):
+                if j < len(arr[x]):
+                    sum_v += arr[x][j]
+                    count += 1
+                if count == M:
+                    max_value = max(max_value, sum_v)
+                    min_value = min(min_value, sum_v)
+                    break
 
+    if max_value == float('-inf'):
+        max_value = -1
 
-    for i in range(len(col)):
-        for j in range(1, len(col)):
-            pass
-
-    # ing
+    print(f'#{tc} {max_value - min_value}')

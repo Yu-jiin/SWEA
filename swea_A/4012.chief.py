8faab7a53d86d63641dae25095e44be4582b89d6
Yujiin <jiin4083@naver.com>
2024-09-02 22:03:18 +0900

    0902 done

--- swea_A/4012.chief.py
+++ swea_A/4012.chief.py
@@ -1,22 +1,35 @@
 import sys
 sys.stdin = open('4012.txt')
 
-def check_food(start, min_sum):
-    for i in range(1, N):
-        for j in range(1, N):
-            if i == j:
-                continue
+
+def dfs(n, k):
+    global min_food
+    if k == N // 2:
+        one, two = 0, 0
+        for i in range(N-1):
+            for j in range(i+1, N):
+                if visited[i] and visited[j]:
+                    one += arr[i][j] + arr[j][i]
+                elif not visited[i] and not visited[j]:
+                    two += arr[i][j] + arr[j][i]
+        min_food = min(min_food, abs(one-two))
+
+    for x in range(n, N):
+        if not visited[x]:
+            visited[x] = 1
+            dfs(x+1, k+1)
+            visited[x] = 0
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     padding = [0] * (N + 1)
-    arr = [[0] + list(map(int, input().split())) for _ in range(N)]
-    arr.insert(0, padding)
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    visited = [0] * N
 
-    min_food = float('inf')
-    # 식재료 선택 경우의 수 만들기
+    min_food = 100000000000
 
-    food1 = check_food(1, 0)
-    food2 = check_food(1, 0)
+    dfs(0,0)
+
+    print(f'#{tc} {min_food}')

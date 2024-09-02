786c0b0fca57dc8da9bb2c8b330351b856141ea0
Yu-jiin <jiin4083@naver.com>
2024-09-02 14:14:08 +0900

    반복&재귀

---
+++ 20240902_완전탐색/11_33.py
@@ -0,0 +1,27 @@
+# 11~33까지 출력 1,2,3으로만
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(f'{i}{j}')
+
+# 1111~3333까지 출력 하려면
+# 위에서 for 두번 더 쓰면 됨 !!
+
+# 그러면 1111111111111~3333333333333은 반복문 저만치 쓸래 ?
+# 좋은말로 할 때 재귀로
+path = []
+N = 3
+
+
+def run(lev):
+    if lev == N:
+        print(path)
+        return
+
+    for i in range(1, 4):
+        path.append(i)
+        run(lev+1)
+        path.pop()
+
+
+run(0)

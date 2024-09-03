0d7e98048a9951746f443076cf38533aa6500490
Yu-jiin <jiin4083@naver.com>
2024-09-03 16:15:28 +0900

    5201 컨테이너 운반 done

--- 05_완전검색_그리디_II/5201_컨테이너_운반/sol.py
+++ 05_완전검색_그리디_II/5201_컨테이너_운반/sol.py
@@ -1,2 +1,25 @@
 import sys
 sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())        # N - 컨테이너 수  M - 트럭 수
+    arr = list(map(int, input().split()))   # 화물 무게
+    truck = list(map(int, input().split())) # 트럭 적재용량
+
+    arr.sort()
+    truck.sort()
+    result = 0
+
+    while truck:
+        if truck and arr:
+            if truck[-1] >= arr[-1]:
+                truck.pop()
+                result += arr.pop()
+            else:
+                arr.pop()
+        else:
+            break
+            print(f'#{tc} {result}')
+
+    print(f'#{tc} {result}')

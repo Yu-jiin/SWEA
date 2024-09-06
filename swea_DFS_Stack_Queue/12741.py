f23d844d232c4a948212b3e9f116c8bd21b14b3a
Yu-jiin <jiin4083@naver.com>
2024-09-06 12:27:40 +0900

    전구문제 진짜 문제 쓰레기

---
+++ swea_DFS_Stack_Queue/12741.py
@@ -0,0 +1,24 @@
+'''
+3
+1 3 5 7
+0 5 2 4
+0 5 1 6
+'''
+
+T = int(input())
+for tc in range(1, T+1):
+    arr = list(map(int, input().split()))
+
+    A = list(range(arr[0], arr[1]))
+    B = list(range(arr[2], arr[3]))
+
+    result = 0
+    answer = []
+    for i in range(len(A)):
+        for j in range(len(B)):
+            if A[i] == B[j]:
+                result += 1
+    answer.append(result)
+
+    for i in range(len(answer)):
+        print(f'#{tc} {answer[i]}')
